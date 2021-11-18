7ef95a3fbbc93fc375bc4a89a45b79c0128b6cf4
deaaprizal <dea.afrizal@icloud.com>
2021-11-19 01:06:29 +0700

    update rules

---
+++ src/components/footline.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footline() {
+  return (
+    <div className="icons-social mt-5">
+      <p className="h6">Learn On Work Environment Vibes</p>
+    </div>
+  );
+}
