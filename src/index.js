50b76d3644599c220ed2cab6857dbc37c6f7f2a9
deaaprizal <dea.afrizal@icloud.com>
2021-11-19 10:09:00 +0700

    fix conf

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import "./style/index.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'babel-polyfill'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "babel-polyfill";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
+);
